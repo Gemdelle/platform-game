3808f8ebc1f35ee9096ff393482ba631023affe1
Gemdelle <milu200911@hotmail.com>
2024-08-02 20:56:45 -0300

    popup styles.

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -4,11 +4,19 @@ import '../popup.css';
 
 const Support = ({ onClose }) => {
   return (
-    <div className='support popup'>
+    <div className='support popup bg'>
       <div className='support-content'>
-        <button onClick={onClose} className='close-btn'>Close</button>
-        <h2>Support</h2>
-        <p>Your Support content goes here.</p>
+        <button onClick={onClose} className='close-btn'></button>
+        <div className='support-text'>
+          <h2>Support</h2>
+          <p>Your Support content goes here.</p>
+        </div>
+        <div className='support-text-area flex'>
+        <textarea className='support-message' placeholder='Write your comment...'></textarea>
+        </div>
+        
+        <div className='support-submit flex'>
+          <button type='submit' className='submit-btn bg'>SUBMIT</button></div>
       </div>
     </div>
   );
