267bc0b8ef8e2495a367b6e1138b480a17a1c450
Tessue <gabialessandroledesma@gmail.com>
2024-08-03 03:55:27 -0300

    pet gif

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Preview.css';
 const Preview = () => {
     return (
-        <div className='preview'>
+        <div className='preview bg'>
 
         </div>
     );
