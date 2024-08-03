b5ed0b25dc87ad8fe36d9be320f838d54e1ff988
Gemdelle <milu200911@hotmail.com>
2024-08-03 18:44:32 -0300

    course-ui.

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Course.css';
-import {useNavigate} from "react-router-dom";
-const Course = ({name, description:{ message, itemCount }, levels = 1, navigationUrl}) => {
+import { useNavigate } from "react-router-dom";
+const Course = ({ name, description: { message, itemCount }, levels = 1, navigationUrl }) => {
     let itemsRange = Array.from({ length: itemCount }, (_, index) => index + 1);
     let levelsRange = Array.from({ length: levels }, (_, index) => index + 1);
 
@@ -12,19 +12,27 @@ const Course = ({name, description:{ message, itemCount }, levels = 1, navigatio
     };
     return (
         <div className='course' id='course-1'>
-            <h1>{name}</h1>
-            <div className='course-description'>
-                <span>{message}</span>
-                <ul>
-                    {itemsRange.map((number, index) => {
-                        return (<div key={`${name}-${index}`}><div className='heart-bullet bg'></div><li>Item {number}</li></div>);
+            <div className='course-container flex-c'>
+                <div className='corners'>
+                    <div className='corner-1'></div>
+                    <div className='corner-2'></div>
+                    <div className='corner-3'></div>
+                    <div className='corner-4'></div>
+                </div>
+                <h1>{name}</h1>
+                <div className='course-description'>
+                    <span>{message}</span>
+                    <ul>
+                        {itemsRange.map((number, index) => {
+                            return (<div key={`${name}-${index}`}><div className='heart-bullet bg'></div><li>Item {number}</li></div>);
+                        })}
+                    </ul>
+                </div>
+                <div className='levels'>
+                    {levelsRange.map((number) => {
+                        return (<div className='level flex bg' onClick={() => { navigateToCourse(number) }}><span>{number}</span></div>);
                     })}
-                </ul>
-            </div>
-            <div className='levels'>
-                {levelsRange.map((number) => {
-                    return (<div className='level flex bg' onClick={()=>{navigateToCourse(number)}}><span>{number}</span></div>);
-                })}
+                </div>
             </div>
         </div>
     );
