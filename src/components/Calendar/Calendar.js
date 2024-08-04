33f930e24bb1ee457b715a246284ca296d791e96
Tessue <gabialessandroledesma@gmail.com>
2024-08-04 04:07:19 -0300

    update calendar.

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './Calendar.css';
 
-const days = ["M","T","W","T","F","S","S"];
-const exercises = [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 11.0, 12.1, 13.2, 14.3, 15.4, 16.5, 17.6, 18.7, 19.8, 20.9, 22.0, 23.1, 24.2, 25.3, 26.4, 27.5, 28.6, 29.7, 30.8, 31.9, 33.0, 34.1,35.35]
+const days = ["M", "T", "W", "T", "F", "S", "S"];
+const exercises = [
+  1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10,
+  11.11, 12.12, 13.13, 14.14, 15.15, 16.16, 17.17, 18.18, 19.19, 20.20,
+  21.21, 22.22, 23.23, 24.24, 25.25, 26.26, 27.27, 28.28, 29.29, 30.30,
+  31.31, 32.32, 33.33, 34.34, 35.35
+];
 
 let dates = [];
 for (let i = 1; i <= 35; i++) {
   dates.push(i);
 }
 
-
 const Calendar = ({ onClose }) => {
   return (
     <div className='calendar-popup bg'>
+      <p className='calendar-text'>Calendar</p>
+      <p className='month-text'>Mes</p>
       <button onClick={onClose} className='close-btn'></button>
       <div className="calendar">
         <div className='week-days'>
@@ -24,7 +30,7 @@ const Calendar = ({ onClose }) => {
           {dates.map((date, index) => (
             <div className="day" key={index}>
               <div className="date"><span>{date}</span></div>
-              <div className="exercise"><span>{exercises}</span></div>
+              <div className="exercise"><span>{exercises[index].toFixed(1)}</span></div>
             </div>
           ))}
         </div>
