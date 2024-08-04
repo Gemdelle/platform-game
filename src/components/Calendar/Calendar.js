bd137cb7a2f9d34af86a3595c996154a88058d9e
Tessue <gabialessandroledesma@gmail.com>
2024-08-04 01:02:33 -0300

    update calendar.

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Calendar.css';
 
+const days = ["M","T","W","T","F","S","S"];
+
 const Calendar = ({ onClose }) => {
   return (
     <div className='calendar-popup bg'>
       <button onClick={onClose} className='close-btn'></button>
       <div className="calendar">
-      <div className='week-days'></div>
+      <div className='week-days'>
+        <div className="day-container">{days[0]}</div>
+        <div className="day-container">{days[1]}</div>
+        <div className="day-container">{days[2]}</div>
+        <div className="day-container">{days[3]}</div>
+        <div className="day-container">{days[4]}</div>
+        <div className="day-container">{days[5]}</div>
+        <div className="day-container">{days[6]}</div>
+      </div>
         <div className="grid">
           {[...Array(5)].map((_, rowIndex) => (
             <div className="row" key={rowIndex}>
