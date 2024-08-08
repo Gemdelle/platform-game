d72d26af72a1591746f6592c2d090ac24aa7ce8a
Hunny <unreal5_gonza@outlook.es>
2024-08-07 23:11:29 -0300

    Agrega logica de niveles.

--- src/screens/Course1/TeoricSublevel/TeoricSublevel.js
+++ src/screens/Course1/TeoricSublevel/TeoricSublevel.js
@@ -157,7 +157,7 @@ const TeoricSublevel = ({backgroundImage}) => {
     const handleFinish = async () => {
         try {
             const idToken = userProfile.id
-            const response = await axios.post('http://localhost:3001/validate/course/1/theoretical', {
+            const response = await axios.post('https://quiet-badlands-42095-c0012ddb8417.herokuapp.com/validate/course/1/theoretical', {
                 theoretical: {
                     score: {
                         current: score,
