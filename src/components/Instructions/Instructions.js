a34a6f69a8b9d5c6a7fe23e3917b438a189734af
Gemdelle <milu200911@hotmail.com>
2024-08-10 20:04:47 -0300

    orbs.

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -4,11 +4,11 @@ const Instructions = () => {
     return (
         <div className="instructions">
             <div className='instructions-container'>
-            <div className='instructions-opener'></div>
+            <div className='instructions-opener'><div className='instructions-orb'></div></div>
             <div className='instructions-text'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </div>
-            <div className='instructions-closer'></div>
+            <div className='instructions-closer'><div className='instructions-orb'></div></div>
             </div>
         </div>
     );
