dc91e7e5eb2f87f22b7d54703cd2bd2ce089f1cf
Hunny <unreal5_gonza@outlook.es>
2024-08-10 15:13:01 -0300

    Arregla codigo sublevels.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './screens/Home/Home';
-import Course1Sublevel1 from "./screens/Course1/Course1Sublevel1/Course1Sublevel1";
+import Course1Sublevel1 from "./screens/Course1/Course1Sublevel1";
 import AvatarSelection from "./screens/AvatarSelection/AvatarSelection";
-import Course1Sublevel2 from "./screens/Course1/Course1Sublevel2/Course1Sublevel2";
-import Course1Sublevel3 from "./screens/Course1/Course1Sublevel3/Course1Sublevel3";
+import Course1Sublevel2 from "./screens/Course1/Course1Sublevel2";
+import Course1Sublevel3 from "./screens/Course1/Course1Sublevel3";
 import TeoricSublevel from "./screens/Course1/TeoricSublevel/TeoricSublevel";
 
 
