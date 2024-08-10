cc09a662e49af49d62f320be4ae46d2cdaab97be
Hunny <unreal5_gonza@outlook.es>
2024-08-10 20:58:17 -0300

    Asigna la orbe que corresponde segun el tipo.

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Instructions.css';
+import {useUser} from "../utils/UserProvider";
 const Instructions = () => {
+    const {userProfile} = useUser();
+    const isAquatic = userProfile.profile.avatar === 'axolotl';
+    const isAerial = userProfile.profile.avatar === 'caterpillar';
+    const isTerrestrial = userProfile.profile.avatar === 'pterodactyl';
     return (
         <div className="instructions">
             <div className='instructions-container'>
-            <div className='instructions-opener'><div className='instructions-orb'></div></div>
+            <div className='instructions-opener'><div className={`instructions-orb ${isAquatic ? 'aquatic' : isTerrestrial ? 'terrestrial' : 'aerial'}`}></div></div>
             <div className='instructions-text'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </div>
-            <div className='instructions-closer'><div className='instructions-orb'></div></div>
+            <div className='instructions-closer'><div className={`instructions-orb ${isAquatic ? 'aquatic' : isTerrestrial ? 'terrestrial' : 'aerial'}`}></div></div>
             </div>
         </div>
     );
