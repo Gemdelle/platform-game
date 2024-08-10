6883bea72e911b89872c57e75efdc08afc3fb645
Gemdelle <milu200911@hotmail.com>
2024-08-10 18:32:17 -0300

    background img.

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -3,7 +3,9 @@ import './Instructions.css';
 const Instructions = () => {
     return (
         <div className="instructions">
+            <div className='instructions-text'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
         </div>
     );
 };
