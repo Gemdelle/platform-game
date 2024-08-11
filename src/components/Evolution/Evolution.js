6bb81da211c927af5f4d6e91348afd6b2482aa10
Hunny <unreal5_gonza@outlook.es>
2024-08-11 20:25:13 -0300

    evolution timer.

--- src/components/Evolution/Evolution.js
+++ src/components/Evolution/Evolution.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Evolution.css';
 
 
-const Evolution = ({ onClose }) => {
-  return (
-    <div className='evolution-popup bg'>
-      <p className='evolution-text'>Event</p>
-      <button onClick={onClose} className='close-btn-calendar'></button>
-      <div className="evolution">
-        <div className="pet-evolution"></div>
-        <span>Your Pet is Evolving</span>
-      </div>
-    </div>
-  );
+const Evolution = ({onClose}) => {
+    useEffect(() => {
+        setTimeout(() => {
+            onClose();
+        }, 13000)
+    }, []);
+    return (
+        <div className='evolution-popup bg'>
+            <p className='evolution-text'>Event</p>
+            <div className="evolution">
+                <div className="pet-evolution"></div>
+                <span>Your Pet is Evolving</span>
+            </div>
+        </div>
+    );
 };
 
 export default Evolution;
