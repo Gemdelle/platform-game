93d5fb366bed991dbd04011fc3174233c6d770bc
Hunny <unreal5_gonza@outlook.es>
2024-08-16 23:34:22 -0300

    Agrega comportamiento de completar slides.

--- src/components/Library/Course1/Slides.js
+++ src/components/Library/Course1/Slides.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import '../Library.css';
 import './Models.css';
 
-const Slides = ({ onClose, subLevel:{ title, slides} }) => {
+const Slides = ({ onClose, subLevel: { title, slides }, levelIndex, subLevelIndex, updateSlideCompletion, totalSlides, completedSlides }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const handleLeftClick = () => {
@@ -12,16 +12,25 @@ const Slides = ({ onClose, subLevel:{ title, slides} }) => {
     const handleRightClick = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
     };
+
+    const handleCompleteClick = () => {
+        updateSlideCompletion(levelIndex, subLevelIndex, currentIndex);
+    };
+
     return (
         <div className='library-popup'>
             <div className='library-container'>
                 <button onClick={onClose} className='close-btn-library'></button>
                 <div className='progress-container'>
                     <div className='progress-and-button'>
-                        <div className='button'></div>
+                        <div
+                            className={`button ${slides[currentIndex].completed ? 'completed' : ''}`}
+                            onClick={handleCompleteClick}
+                        >
+                        </div>
                         <div className='progress'>
-                            <span className='correct'>28</span>
-                            <span className='total'>29</span>
+                            <span className='correct'>{completedSlides}</span>
+                            <span className='total'>{totalSlides}</span>
                         </div>
                     </div>
                 </div>
@@ -29,7 +38,7 @@ const Slides = ({ onClose, subLevel:{ title, slides} }) => {
                     <div className='library-index'>{currentIndex + 1}</div>
                 </div>
                 <div className='card-content'>
-                    <div >
+                    <div>
                         <div>{title}</div>
                         <div>{slides[currentIndex].subtitle}</div>
                         {slides[currentIndex].texts}
@@ -41,7 +50,8 @@ const Slides = ({ onClose, subLevel:{ title, slides} }) => {
             </div>
             <div className='library-arrows'>
                 {currentIndex !== 0 ? <div className='arrow-left' onClick={handleLeftClick}></div> : null}
-                {currentIndex !== slides.length -1 ? <div className='arrow-right' onClick={handleRightClick}></div> : null}
+                {currentIndex !== slides.length - 1 ?
+                    <div className='arrow-right' onClick={handleRightClick}></div> : null}
             </div>
         </div>
     );
